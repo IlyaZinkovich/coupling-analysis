61750d09e9d01167da9f935aa6b83177d55fd728
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-12-29 19:49:51 +0300

    Added receiver coupling test

--- src/test/java/io/analysis/coupling/OutboundDependenciesTest.java
+++ src/test/java/io/analysis/coupling/OutboundDependenciesTest.java
@@ -10,12 +10,22 @@ import static org.junit.Assert.assertEquals;
 public class OutboundDependenciesTest {
 
     @Test
-    public void test() throws IOException, URISyntaxException {
+    public void testCallerEfferentCoupling() throws IOException, URISyntaxException {
         String callerSourceFilePath = this.getClass().getResource("classes/Caller.class").getPath().substring(1);
         InstrumentedClass instrumentedClass = new InstrumentedClass(callerSourceFilePath);
         OutboundDependencies outboundDependencies = new OutboundDependencies(instrumentedClass);
         EfferentCoupling efferentCoupling = outboundDependencies.efferentCoupling();
         assertEquals("io/analysis/coupling/classes/Caller", efferentCoupling.className());
+        assertEquals(3, efferentCoupling.value());
+    }
+
+    @Test
+    public void testReceiverEfferentCoupling() throws IOException, URISyntaxException {
+        String callerSourceFilePath = this.getClass().getResource("classes/Receiver.class").getPath().substring(1);
+        InstrumentedClass instrumentedClass = new InstrumentedClass(callerSourceFilePath);
+        OutboundDependencies outboundDependencies = new OutboundDependencies(instrumentedClass);
+        EfferentCoupling efferentCoupling = outboundDependencies.efferentCoupling();
+        assertEquals("io/analysis/coupling/classes/Receiver", efferentCoupling.className());
         assertEquals(3, efferentCoupling.value());
     }
 }
